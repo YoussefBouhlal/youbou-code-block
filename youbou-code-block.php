e9575d571f40c38f8223055ed6f1376de2934b04
youssef bouhlal <pro.youssef.bouhlal@gmail.com>
2024-07-10 09:27:59 +0100

    fix: change Author URI due to review in wp repository

--- youbou-code-block.php
+++ youbou-code-block.php
@@ -8,7 +8,7 @@
  * Requires at least: 6.1
  * Requires PHP:      7.2
  * Author:            Youssef Bouhlal
- * Author URI:        https://youssefbouhlal.com
+ * Author URI:        pro.youssef.bouhlal@gmail.com
  * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       youboucodeblock
